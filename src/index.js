30fff207d42782d3a26c8ceaf6ba593d4dee900e
Krasimir Dimitrov <133798641+krasi-pd-bg@users.noreply.github.com>
2024-09-14 17:32:46 +0300

    main layout added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
 const path = require('path');
+const routes = require('./routes');
 
 const app = express();
 const port = 5000;
@@ -11,9 +12,7 @@ app.engine("hbs", handlebars.engine({
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(routes);
 
-app.get('/', (req, res) => {
-    res.render("home", {layout: false});
-});
 
 app.listen(port, () => console.log(`Server is listening on port ${port}...`));
