f05d257a9a82fe9434e35d5cebd066d7a8013c40
Krasimir Dimitrov <133798641+krasi-pd-bg@users.noreply.github.com>
2024-09-14 10:58:38 +0300

    resources added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log("init");
+const express = require('express');
+const handlebars = require('express-handlebars');
+const path = require('path');
+
+const app = express();
+const port = 5000;
+
+app.engine("hbs", handlebars.engine({
+    extname: "hbs",
+}));
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get('/', (req, res) => {
+    res.render("home", {layout: false});
+});
+
+app.listen(port, () => console.log(`Server is listening on port ${port}...`));
