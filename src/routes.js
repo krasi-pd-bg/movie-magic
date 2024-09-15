2c16a18a6453d601365978f2f14ced5aac75771f
Krasimir Dimitrov <133798641+krasi-pd-bg@users.noreply.github.com>
2024-09-15 15:10:08 +0300

    create page added

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const homeController = require('./controllers/homeController');
+const movieController = require('./controllers/movieController');
 
 router.use(homeController);
+router.use(movieController);
 
-module.exports = router;
+module.exports =  router ;
