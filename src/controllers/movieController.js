2f1cbeac2e77f71466a3387fc34c08a80214ddd7
Krasimir Dimitrov <133798641+krasi-pd-bg@users.noreply.github.com>
2024-09-22 17:10:49 +0300

    details page added

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -1,7 +1,26 @@
 const router = require('express').Router();
+const movieService = require('../services/movieService');
 
-router.get('/create', function (req, res) {
+router.get('/create', function (req, res)  {
     res.render('create');
+});
+
+router.post('/create', function (req, res) {
+    const newMovie = req.body;
+    movieService.create(newMovie);
+
+    res.redirect('/');
+    
+});
+
+router.get('/movies/:movieId', (req, res) => {
+    const movieId = req.params.movieId;
+    const movie = movieService.getOne(movieId);
+    
+    // TODO: This is not perfect, use handlebars helpers
+    //movie.rating = new Array(Number(movie.rating)).fill(true);
+    
+    res.render('details', { movie });
 });
 
 module.exports = router;
