9b207f5838fc7cd4fa0f87211d16367e4a733e1e
Krasimir Dimitrov <133798641+krasi-pd-bg@users.noreply.github.com>
2024-09-23 16:05:21 +0300

    all done

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -12,6 +12,13 @@ router.get('/about', (req, res) => {
     res.render("about");
 });
 
+router.get('/search', (req, res) => {
+    const { title, genre, year } = req.query;
+    const movieResult = movieService.search(title, genre, year);
+
+    res.render('search', { movies: movieResult, title, genre, year });
+});
+
 router.get('/404', (req, res) => {
     res.render('404');
 });
